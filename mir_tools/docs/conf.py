4dd32e5f069fb322c0fd50c7a02225efde28dc51
Rayssa Guimaraes Silva <rayssa17@astro.ufrj.br>
2022-10-16 00:34:42 -0300

    Fixing theme again

--- mir_tools/docs/conf.py
+++ mir_tools/docs/conf.py
@@ -23,7 +23,7 @@ author = 'Rayssa Guimarães Silva'
 root_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -47,8 +47,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'furo'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
