88139c5f9c4cbbcc6adcc0201f61faed474404ca
Rayssa Guimaraes Silva <rayssa17@astro.ufrj.br>
2022-10-16 00:31:22 -0300

    Gave up on Furo for now

--- mir_tools/docs/conf.py
+++ mir_tools/docs/conf.py
@@ -31,7 +31,7 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','furo']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +47,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+#html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
